5eb3a1c29f687230b61514f92f1c47333bcbd569
Andrey Piterov <pfm@live.ru>
2017-09-11 21:58:28 +0300

    scene layout

---
+++ ChatApp/ChatViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ChatViewController.swift
+//  ChatApp
+//
+//  Created by Andrew Piterov on 11/09/2017.
+//  Copyright © 2017 Andrew Pierov. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+     let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+     
+     cell.textLabel?.text = "Test"
+     
+     return cell
+     }
+ 
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
